7a99f9a318112d7d660b4b5f1501c05b332e0c55
Liam Borella <ulfrthewolf@gmail.com>
2021-12-05 17:20:37 +1000

    Added some comments to dayfive.fs because I felt like it

--- fsharp/dayfive.fs
+++ fsharp/dayfive.fs
@@ -5,6 +5,16 @@ open Core
 type Point = int * int
 type Line = Point * Point
 
+(*
+Unoptimized, bad way of solving day 5. The better way (which uses more RAM
+but like not much) would be to create an actual board of points and plot
+each line on the board.
+
+This solution checks each pair of lines on the board and calculates their
+intersection points, and counts the number of unique ones.
+*)
+
+// Functions for parsing lines from file
 let pointFromString (str:string) =
     let splits = str.Split(",")
     (int splits.[0], int splits.[1])
@@ -12,18 +22,30 @@ let pointFromString (str:string) =
 let lineFromStrings (strs:string[]) =
     (pointFromString strs.[0], pointFromString strs.[1])
 
+let linesFromFile filename =
+    readLines filename 
+    |> List.map (fun s -> lineFromStrings (s.Split(" -> ")))
+
+// This could probably go into Core soon but I wrote it for this
+// Generates a list of all the unordered pairs of a list 
+// (excluding pairs of an element with itself i.e. (x,x))
+let rec unorderedPairs (lst: 'a list) =
+    match lst with
+    | [_] | [] -> []
+    | x::xs -> (List.map (fun y -> (x, y)) xs) @ (unorderedPairs xs)
+
+// Flattens a list 1 level. E.g. [[1;2];[3;4]] -> [1;2;3;4]
+let flatten = List.fold (@) []
+
 let isLineStraight (line:Line) =
     let start = fst line
     let ending = snd line
     
     (fst start = fst ending) || (snd start = snd ending)
 
-let rec unorderedPairs (lst: 'a list) =
-    match lst with
-    | [_] | [] -> []
-    | x::xs -> (List.map (fun y -> (x, y)) xs) @ (unorderedPairs xs)
-
-
+// Generates a list of points the line covers from beginning to end.
+// Somehow i got it to work regardless of direction (but it looks yucky, sorry
+// about that)
 let lineRange line =
     let bounds = fun start ending ->
         if start <= ending then [start .. ending]
@@ -50,7 +72,7 @@ let pointsOfIntersection (lines: Line * Line) =
     line1
     |> List.map (fun p1 -> List.filter (fun p2 -> p2 = p1) line2)
     |> List.filter (fun lst -> lst <> [])
-    |> List.fold (@) [] // Flatten list
+    |> flatten
 
 let generalSolution filename part =
     let tautology = fun x -> true
@@ -58,14 +80,12 @@ let generalSolution filename part =
     let filter = if part = 1 then isLineStraight
                  else tautology
 
-    readLines filename 
-    |> List.map (fun s -> s.Split(" -> "))
-    |> List.map lineFromStrings
+    linesFromFile filename
     |> List.filter filter
     |> unorderedPairs 
     |> List.map pointsOfIntersection
     |> List.filter (fun lst -> lst <> [])
-    |> List.fold (@) []
+    |> flatten
     |> List.countBy id
     |> List.length
 
